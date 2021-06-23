039a32ae7cdad2d57f4e380c2635be83f195a152
Danilo Borozan <69689353+daniloborozan1998@users.noreply.github.com>
2021-06-23 12:05:07 +0200

    Homework 03

---
+++ Homework03/SQLQueryHomeworkRequirement1.sql
@@ -0,0 +1,30 @@
+--HOMEWORK REQUIREMENT 1/3
+
+--Calculate the count of all grades in the system
+
+SELECT COUNT(Grade) as TOTAL 
+FROM [dbo].[Grade]
+GO
+
+--Calculate the count of all grades per Teacher in the system
+
+SELECT TeacherID, COUNT(Grade) as TOTAL
+FROM [dbo].[Grade]
+GROUP BY TeacherID
+GO
+
+--Calculate the count of all grades per Teacher in the system for first 100 Students (ID < 100)
+
+
+SELECT TeacherID, COUNT(Grade) as TOTAL
+FROM [dbo].[Grade]
+WHERE StudentID < 100
+GROUP BY TeacherID
+GO
+
+--Find the Maximal Grade, and the Average Grade per Student on all grades in the system
+
+SELECT TeacherID, MAX(Grade) as TOTAL, AVG(Grade) as AVERAGE
+FROM [dbo].[Grade]
+GROUP BY TeacherID
+GO
---
+++ Homework03/SQLQueryHomeworkRequirement3.sql
@@ -0,0 +1,43 @@
+--Homework requirement 3/3
+
+--Create new view (vv_StudentGrades) that will List all StudentIdsand count of Grades per student
+
+DROP VIEW IF EXISTS vv_StudentGrades;
+GO
+
+CREATE VIEW vv_StudentGrades
+AS
+SELECT StudentID, COUNT(Grade) AS COUNTS
+FROM [dbo].[Grade]
+GROUP BY StudentID
+GO
+
+--Change the view to show Student First and Last Names instead of StudentID
+
+ALTER VIEW vv_StudentGrades
+AS
+SELECT S.FirstName,S.LastName, COUNT(Grade) AS COUNTS
+FROM [dbo].[Grade] G
+INNER JOIN [dbo].[Student] S ON G.StudentID = S.ID
+GROUP BY S.FirstName, S.LastName
+
+--List all rows from view ordered by biggest Grade Count
+
+SELECT *
+FROM vv_StudentGrades
+ORDER BY COUNTS DESC
+GO
+
+--Create new view (vv_StudentGradeDetails) that will List all Students (FirstName and LastName) and Count the courses he passed through the exam(Ispit)
+
+CREATE VIEW vv_StudentGradeDetails
+AS
+SELECT S.FirstName + N' ' + S.LastName AS STUDENTSNAME, COUNT(C.Name) AS EXAM
+FROM [dbo].[Grade] G
+INNER JOIN [dbo].[Student] S ON G.StudentID = S.ID
+INNER JOIN [dbo].[Course] C ON C.ID = G.CourseID
+GROUP BY S.FirstName,S.LastName
+
+
+
+
