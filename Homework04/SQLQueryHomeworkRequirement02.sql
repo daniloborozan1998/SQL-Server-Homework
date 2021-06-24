aa3613acfd0c42d906aa9d648304a5da7c18ec4f
Danilo Borozan <daniloborozan07@gmail.com>
2021-06-24 14:48:13 +0200

    Homework04
    Homework04

---
+++ Homework04/SQLQueryHomeworkRequirement02.sql
@@ -0,0 +1,26 @@
+﻿-- Homework requirement 2/2
+
+-- Declare scalar function (fn_FormatStudentName) for retrieving the Student description for specific StudentId in the following format:
+	--StudentCardNumber without “sc-”
+	--“ – “
+	--First character  of student  FirstName
+	--“.”
+	--Student LastName
+
+CREATE FUNCTION dbo.fn_FormatStudentNames (@StudentId int)
+RETURNS NVARCHAR(100)
+AS
+BEGIN
+
+DECLARE @Results NVARCHAR(100)
+
+SELECT @Results = RIGHT(StudentCardNumber,5) + N'-' + LEFT(FirstName,1) + N'.' + LastName
+FROM Student
+WHERE ID = @StudentId
+RETURN @Results
+END
+GO
+
+SELECT *, dbo.fn_FormatStudentNames (ID) AS FUNCTIONOUTPUT
+FROM Student
+
---
+++ Homework04/SQLQueryHomeworkRequirement1.sql
@@ -0,0 +1,54 @@
+﻿-- Homework requirement 1/2
+
+--Declare scalar variable for storing FirstName values
+   --Assign value ‘Antonio’ to the FirstName variable
+   --Find all Students  having FirstName  same as the variable
+
+DECLARE @FirstName NVARCHAR(100)
+SET @FirstName = 'Antonio'
+
+SELECT *
+FROM Student
+WHERE FirstName = @FirstName
+
+--Declare table variable that will contain StudentId, StudentName and DateOfBirth
+	--Fill the  table variable with all Female  students
+
+
+DECLARE @StudentList TABLE
+(StudentId int, FirstName NVARCHAR(100), DateOfBirth date);
+
+INSERT INTO @StudentList
+SELECT ID,FirstName,DateOfBirth
+FROM Student
+WHERE Gender = 'F'
+
+SELECT *
+FROM @StudentList
+GO
+
+--Declare temp table that will contain LastName and EnrolledDate columns
+	--Fill the temp table with all Male students having First Name starting with ‘A’
+	--Retrieve  the students  from the  table which last name  is with 7 characters
+
+CREATE TABLE #StudentList
+(LastName NVARCHAR(100), EnrolledDate date)
+
+INSERT INTO #StudentList
+SELECT LastName, EnrolledDate
+FROM Student
+WHERE FirstName LIKE 'A%'
+
+SELECT *
+FROM #StudentList
+WHERE LEN(LastName) = 7
+
+-- Find all teachers whose FirstName length is less than 5 and the first 3 characters of their FirstName  and LastNameare the same
+
+CREATE TABLE #TeacherList
+(FirstName NVARCHAR(100),LastName NVARCHAR(100))
+
+INSERT INTO #TeacherList
+SELECT FirstName, LastName
+FROM Teacher
+WHERE SUBSTRING(FirstName,1,3) = SUBSTRING(LastName,1,3) and LEN(FirstName) < 5
