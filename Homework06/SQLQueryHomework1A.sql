d372a85e99e40f733fbf4cfd035a4011250abe1c
Danilo Borozan <daniloborozan07@gmail.com>
2021-06-28 17:46:37 +0200

    Homework06

--- Homework06/SQLQueryHomework1A.sql
+++ Homework06/SQLQueryHomework1A.sql
@@ -7,11 +7,11 @@
 --Procedure should return second resultset with the MAX Grade of all grades for the Student and Teacher on input (regardless the Course)
 
 
-ALTER PROCEDURE dbo.CreateGrade(@StudentId int, @CourseId int, @TeacherId int)
+ALTER PROCEDURE dbo.CreateGrade(@StudentId int, @CourseId int, @TeacherId int,@CreatedDate date, @Grade int)
 AS
 BEGIN
-	INSERT INTO [dbo].[Grade] (StudentId, CourseId, TeacherId)
-	VALUES (@StudentId, @CourseId, @TeacherId)
+	INSERT INTO [dbo].[Grade] (StudentId, CourseId, TeacherId,CreatedDate,Grade)
+	VALUES (@StudentId, @CourseId, @TeacherId, @CreatedDate,@Grade)
 
 	SELECT COUNT(*) AS TotalGradesForStudents
 	FROM [dbo].[Grade] G
@@ -24,7 +24,7 @@ BEGIN
 END
 GO
 
-EXEC dbo.CreateGrade @StudentId=291, @CourseId=6,@TeacherId=85
+EXEC dbo.CreateGrade @StudentId=291, @CourseId=6,@TeacherId=85,@CreatedDate='1999-02-19',@Grade=7
 
 SELECT *
 FROM [dbo].[Grade]
