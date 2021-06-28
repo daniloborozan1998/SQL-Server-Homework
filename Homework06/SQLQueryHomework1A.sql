aeab4f32885c1ecb5721f6996c6bbd208ba9a5be
Danilo Borozan <daniloborozan07@gmail.com>
2021-06-28 16:31:54 +0200

    Homework06

---
+++ Homework06/SQLQueryHomework1A.sql
@@ -0,0 +1,34 @@
+--Homework requirement 1a/2
+
+
+--Create new procedure called CreateGrade
+--Procedure should create only Grade header info (not Grade Details) 
+--Procedure should return the total number of grades in the system for the Student on input (from the CreateGrade)
+--Procedure should return second resultset with the MAX Grade of all grades for the Student and Teacher on input (regardless the Course)
+
+
+ALTER PROCEDURE dbo.CreateGrade(@StudentId int, @CourseId int, @TeacherId int)
+AS
+BEGIN
+	INSERT INTO [dbo].[Grade] (StudentId, CourseId, TeacherId)
+	VALUES (@StudentId, @CourseId, @TeacherId)
+
+	SELECT COUNT(*) AS TotalGradesForStudents
+	FROM [dbo].[Grade] G
+	WHERE StudentID = @StudentId
+
+	SELECT MAX(Grade) AS MAX_GRADE
+	FROM [dbo].[Grade] G
+	WHERE StudentID = @StudentId AND TeacherID = @TeacherId
+
+END
+GO
+
+EXEC dbo.CreateGrade @StudentId=291, @CourseId=6,@TeacherId=85
+
+SELECT *
+FROM [dbo].[Grade]
+
+
+
+
---
+++ Homework06/SQLQueryHomework1B.sql
@@ -0,0 +1,57 @@
+﻿--Homework requirement 1b/2
+
+/*
+--Create new procedure called CreateGradeDetail
+--Procedure should add details for specific Grade (new record for new AchievementTypeID, Points, MaxPoints, Date for specific Grade)
+--Output from this procedure should be resultset with SUM of GradePoints calculated with formula 
+AchievementPoints/AchievementMaxPoints*ParticipationRate for specific Grade.
+*/
+
+/*
+Create new procedure called CreateOrderDetail
+Procedure should add details for specific order (new record for new Product/Quantity for specific order)
+Procedure should take the single price for item from Product table (Price column)
+When the order detail is inserted procedure should correct the TotalPrice column in the main table (dbo.order)
+Output from this procedure should be resultset with order details in a form of pairs: ProductName and TotalPrice per product (Price*Quantity)
+*/
+
+ALTER  PROCEDURE dbo.CreateGradeDetail (@GradeId int, @AchievementTypeID int, @AchievementPointsId int,@AchievementMaxPoints int, @AchievementDate date)
+AS
+BEGIN
+
+DECLARE @ParticipationRate decimal(5,5)
+DECLARE @GradePoints decimal(5,5)
+
+SELECT @ParticipationRate = ParticipationRate
+FROM [dbo].[AchievementType]
+WHERE ID = @AchievementTypeID
+
+INSERT INTO [dbo].[GradeDetails] (GradeId, AchievementTypeID, AchievementPoints, AchievementMaxPoints,AchievementDate)
+VALUES (@GradeId,@AchievementTypeID,@AchievementPointsId,@AchievementMaxPoints,@AchievementDate)
+
+SET @GradePoints  = 
+(
+	SELECT SUM(AchievementPoints/AchievementMaxPoints*A.ParticipationRate)
+	FROM [dbo].[GradeDetails] GD 
+	INNER JOIN [dbo].[AchievementType] A ON A.ID = GD.AchievementTypeID
+	WHERE GD.GradeID = @GradeId 
+)
+
+UPDATE G SET Grade = @GradePoints
+FROM [dbo].[Grade] G
+WHERE ID = @GradeId
+
+SELECT A.Name, SUM(GD.AchievementPoints) AS POINTS, SUM(GD.AchievementPoints/GD.AchievementMaxPoints*A.ParticipationRate) AS TOTALPOINTS
+FROM [dbo].[GradeDetails] GD
+INNER JOIN [dbo].[AchievementType] A ON A.ID = GD.AchievementTypeID
+WHERE GD.GradeID = @GradeId
+GROUP BY A.Name
+
+END
+GO
+
+EXEC dbo.CreateGradeDetail  @GradeId = 4,@AchievementTypeID=12,@AchievementPointsId=81,@AchievementMaxPoints=100,@AchievementDate = '1999-02-02'
+
+SELECT *
+FROM [dbo].[Grade]
+WHERE ID=1
